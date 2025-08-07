31c09df81d25b07a6fd2890160c8f9a13fdf3899
Prudence Levison <you@example.com>
2025-08-07 15:17:43 +0100

    Completed

--- src/app/components/Details.jsx
+++ src/app/components/Details.jsx
@@ -5,6 +5,7 @@ const Details = () => {
     const [names, setNames] =useState('');
     const [messages, setMessages] = useState('');
     const [ submission, setSubmission] =useState(false)
+    const [responsePost, setResponsePost] =useState(null)
 
     const nameHandler=(e)=>{
         setNames(e.target.value)
@@ -21,34 +22,66 @@ const Details = () => {
         setSubmission(true);
         setNames("")
         setMessages("")
-    }
+
+        fetch('https://jsonplaceholder.typicode.com/comments', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: names,
+        body: messages,
+        userId: 1
+      })
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setResponsePost(data); 
+      });
+  };
+
+    
   return (
-    <div className=' flex justify-center '>
+    <>
+    <div className=' flex justify-center pt-5 '>
        
         { !submission && 
+        
       <form onSubmit={submitHandler}
-      className='relative'
+      className=' relative '
+      
      >
         <input
         type='text'
         onChange={nameHandler}
         value={names}
         placeholder='Enter Your Name Here...'
-        className='border-2 bg-white text-gray-600  px-6 py-3' />
+        className='border-2 bg-white text-gray-600 my-2 rounded-lg px-16 py-3' />
 
-        <input 
+        <div className='block'>
+        <textarea
         type='text'
         onChange={messageHandler}
         value={messages}
         placeholder='Whats your story...'
-       className="border-2 bg-white text-gray-600  px-6 py-3  " />
+       className="border-2 bg-white text-gray-600 rounded-lg px-18  py-20  " />
+       </div>
 
         <button type='submit' 
-         className="bg-green-600 border-sm text-white px-3 rounded-sm"> Submit</button>
-      </form>}
+         className="bg-orange-400 border-sm text-white px-34 py-2  rounded-sm"> Submit</button>
+      </form>
+    }
 
-      {submission && <p className='text-white'> Thank you for sharing your story</p>}
-    </div>
+      {submission && <p className='text-white flex justify-center '> Thank you for sharing your story</p>}
+      </div>
+      {responsePost && 
+      <div className=' block text-center'>
+      <p className='text-white py-2 px-2'>Name: {responsePost.name}</p>
+      <p className='text-white'>Message: {responsePost.body}</p>
+      </div>
+      }
+    
+    </>
   )
 }
 
